0c5c38542662474c10a6c69c1d8ee57342e7c21e
Diego Rocha <drochaesc@gmail.com>
2023-09-03 20:19:49 -0600

    Cambiando color de fondo de equipos

--- src/App.js
+++ src/App.js
@@ -9,29 +9,44 @@ import Footer from "./components/Footer"
 function App() {
   const [mostrarFormulario, actualizarMostrar] = useState(false);
   const [colaboradores, actualizarColaboradores] = useState([{
-    equipo: "FrontEnd",
-    foto:"https://github.com/RochaDiego04.png",
-    nombre:"Diego Rocha",
-    puesto:"Desarrollador"
+    equipo: "Front End",
+    foto: "https://github.com/harlandlohora.png",
+    nombre: "Harland Lohora",
+    puesto: "Instructor"
+  },
+  {
+    equipo: "Programación",
+    foto: "https://github.com/RochaDiego04.png",
+    nombre: "Diego Rocha",
+    puesto: "Desarrollador de software e instructor"
+  },
+  {
+    equipo: "UX y Diseño",
+    foto: "https://github.com/JeanmarieAluraLatam.png",
+    nombre: "Jeanmarie Quijada",
+    puesto: "Instructora"
+  },
+  {
+    equipo: "Programación",
+    foto: "https://github.com/christianpva.png",
+    nombre: "Christian Velasco",
+    puesto: "Head e Instructor"
+  },
+  {
+    equipo: "Innovación y Gestión",
+    foto: "https://github.com/JoseDarioGonzalezCha.png",
+    nombre: "Jose Gonzalez",
+    puesto: "Dev FullStack"
   }]);
 
-  const cambiarMostrar = () => {
-    actualizarMostrar(!mostrarFormulario);
-  };
-
-  const registrarColaborador = (colaborador) => {
-    actualizarColaboradores([...colaboradores, colaborador]);
-  }
-
-  //Lista de equipos
-  const equipos = [
+  const [equipos, actualizarEquipos] = useState([
     {
       titulo: "Programación",
       colorPrimario: "#57C278",
       colorSecundario: "#D9F7E9",
     },
     {
-      titulo: "FrontEnd",
+      titulo: "Front End",
       colorPrimario: "#82CFFA",
       colorSecundario: "#E8F8FF",
     },
@@ -60,7 +75,31 @@ function App() {
       colorPrimario: "#FF8A29",
       colorSecundario: "#FFEEDF",
     },
-  ];
+  ])
+
+  const cambiarMostrar = () => {
+    actualizarMostrar(!mostrarFormulario);
+  };
+
+  const registrarColaborador = (colaborador) => {
+    actualizarColaboradores([...colaboradores, colaborador]);
+  }
+
+  const eliminarColaborador = () =>{
+    console.log("eliminando colaborador");
+  }
+
+  //Actualizar color de equipo
+  const actualizarColor = (color, tituloDeEquipo) =>{
+    const equiposActualizados = equipos.map((equipo)=>{
+      if(equipo.titulo === tituloDeEquipo){
+        equipo.colorPrimario = color;
+      }
+      return equipo;
+    })
+
+    actualizarEquipos(equiposActualizados);
+  }
 
   return (
     <div>
@@ -77,7 +116,9 @@ function App() {
           <Equipo 
             datos={equipo} 
             key={equipo.titulo}
-            colaboradores={colaboradores.filter(colaborador => colaborador.equipo === equipo.titulo)} />
+            colaboradores={colaboradores.filter(colaborador => colaborador.equipo === equipo.titulo)}
+            eliminarColaborador={eliminarColaborador}
+            actualizarColor={actualizarColor} />
         ))
       }
       <Footer/>
--- src/components/Equipo/index.js
+++ src/components/Equipo/index.js
@@ -1,10 +1,11 @@
 import "./Equipo.css";
 import Colaborador from "../Colaborador";
+import hexToRgba from "hex-to-rgba";
 
 const Equipo = (props) => {
   //Destructuring
   const { titulo, colorPrimario, colorSecundario } = props.datos;
-  const { colaboradores } = props;
+  const { colaboradores, eliminarColaborador, actualizarColor } = props;
 
   return (
     // condicion ternaria, mostrar si hay colaboradores en el equipo
@@ -12,8 +13,14 @@ const Equipo = (props) => {
       {colaboradores.length > 0 && (
         <section
           className="equipo"
-          style={{ backgroundColor: colorSecundario }}
+          style={{ backgroundColor: hexToRgba(colorPrimario, 0.6) }}
         >
+          <input 
+            type="color"
+            className="input-color"
+            value={colorPrimario}
+            onChange={(e)=> actualizarColor(e.target.value, titulo)}
+            />
           <h3 style={{ borderBottomColor: colorPrimario }}>{titulo}</h3>
           <div className="colaboradores">
             
@@ -21,7 +28,8 @@ const Equipo = (props) => {
               <Colaborador 
                 datos={colaborador} 
                 key={index} 
-                colorPrimario={colorPrimario}/>
+                colorPrimario={colorPrimario}
+                eliminarColaborador={eliminarColaborador}/>
             ))}
             
           </div>
